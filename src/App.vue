3560411e33affebd0642753317a8407951403686
Jason M Harrison <jason.m.harrison@gmail.com>
2020-06-01 10:27:29 -0500

    Updated bg and talk tics.

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,8 @@ export default {
   }
 
   body {
-    background-color: #eae6e6;
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 32 32'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='Artboard-5' fill='%23ffffff' fill-opacity='0.4' fill-rule='nonzero'%3E%3Cpath d='M6 18h12V6H6v12zM4 4h16v16H4V4z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+    background-color: #f7eeac;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='199' viewBox='0 0 100 199'%3E%3Cg fill='%23ef9f7d' fill-opacity='0.32'%3E%3Cpath d='M0 199V0h1v1.99L100 199h-1.12L1 4.22V199H0zM100 2h-.12l-1-2H100v2z'%3E%3C/path%3E%3C/g%3E%3C/svg%3E");
   }
 
   #app {
@@ -42,13 +42,33 @@ export default {
     overflow: hidden;
   }
 
+  button {
+    display:inline-block;
+    padding: 0.5em 2em;
+    border-radius: 5px;
+    opacity: .75;
+    transition: all .2s;
+    text-transform: uppercase;
+  }
+
+  button:hover {
+    opacity: 1;
+    cursor: pointer;
+  }
+
+  @media all and (max-width:30em){
+    button {
+      display: block;
+    }
+  }
+
   footer .emblem {
     height: 30em;
     width: 30em;
     border-radius: 50%;
     margin-top: -3em;
     background-color: #273a82;
-    border: 4px solid white;
+    border: 2em solid white;
     box-shadow: 10px 5px 38px black;
     position: relative;
     text-align: center;
@@ -64,7 +84,7 @@ export default {
   footer {
     display: flex;
     justify-content: center;
-    height: 10em;
+    height: 100vh;
     background: black;
     margin-top: -3em;
     position: relative;
